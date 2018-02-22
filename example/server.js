b93b5f603b3cf4795e8189713c6ed9472e9bd542
Lsong <liusong02@meituan.com>
2018-02-22 18:16:00 +0800

    modify

--- example/server.js
+++ example/server.js
@@ -1,13 +1,12 @@
 'use strict';
-const url  = require('url');
-const http = require('http');
-const WeChat  = require('../');
+const http    = require('http');
+const WeChat  = require('..');
 
-var app = new WeChat.Server('token', function(err, message){
+const app = new WeChat.Server('token', function(err, message){
   // console.log(message);
   switch (message.MsgType) {
     case 'text':
-      return message.Content; // text
+      return message.Content;
       break;
     case 'link':
       return message.Url;
@@ -15,7 +14,6 @@ var app = new WeChat.Server('token', function(err, message){
     case 'location':
       return message.Label;
       break;
-    
     default:
       return message;
       break;
@@ -23,9 +21,3 @@ var app = new WeChat.Server('token', function(err, message){
 });
 
 http.createServer(app).listen(3000);
-
-// const server = http.createServer(WeChat.Server('token', function(err, message){
-//   return message.Content;
-// }));
-
-// server.listen(3000);
