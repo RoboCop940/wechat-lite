864e369ab9f1b2224ec05be0f37d4a4a19738460
Lsong <song940@gmail.com>
2018-03-10 12:13:42 +0800

    add wxapp api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 const WeChat  = require('./lib');
+// expose
 WeChat.Client = require('./lib/client');
 WeChat.Server = require('./lib/server');
+WeChat.MINA   = require('./lib/wxapp');
 /**
  * [exports]
  * @type {[WeChat]}
